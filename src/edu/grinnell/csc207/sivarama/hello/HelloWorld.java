ffc8e30c5b3adb444d56c140ee8134c953a7b062
sivash <sivarama@grinnell.edu>
2014-01-21 19:21:02 -0600

    Update HelloWorld.java

--- src/edu/grinnell/csc207/sivarama/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/sivarama/hello/HelloWorld.java
@@ -1,14 +1,14 @@
- package edu.grinnell.csc207.sivarama.hello;
+package edu.grinnell.csc207.sivarama.hello;
   
-  public class HelloWorld
-    {
-      public static void 
-        main (String[] args) 
-      {
-        System.out.println ("Hello, GitHub second time!");
-        
-         System.out.println ("Hello, GitHub third time!");
-        System.out.println ("Hello, GitHub fourth time!");
-        
-      } // main(String[])
-      }
+public class HelloWorld
+   {
+     public static void 
+     main (String[] args) 
+   {
+     System.out.println ("Hello, GitHub second time!");
+  
+     System.out.println ("Hello, GitHub third time!");
+     System.out.println ("Hello, GitHub fourth time!");
+       
+   } // main(String[])
+  }
